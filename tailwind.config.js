53abaad8a6e6e5bfae79d8dc4a1674b09be4baf3
nicod12 <dumenicolas45@gmail.com>
2024-09-23 10:33:44 -0300

    second update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import animations from "@midudev/tailwind-animations";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [animations],
 };
